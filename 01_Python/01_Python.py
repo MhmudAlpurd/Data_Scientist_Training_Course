5067bf34d7fc0412cc375b87d966d64729e3f637
Mahmoud Alipour <Mahmoudalipour.D@Gmail.Com>
2025-09-11 04:20:01 +0330

    Add files via upload

---
+++ 01_Python/01_Python.py
@@ -0,0 +1,57 @@
+your_mark = 19
+print(your_mark)
+
+your_name = "ali ahmadi"
+print(your_name)
+
+
+#3
+txt = "your mark: "
+mark = 78
+
+print(txt, mark)
+
+#4
+your_field = "football"
+your_field = "wreslting"
+print(your_field)
+
+#5 
+
+var_1 , var_2 , var_3 = 1, 2, 3
+
+print("var1: ", var_1)
+print("var2: ", var_2)
+print("var3: ", var_3)
+
+
+#6
+
+elmnt_1, elmnt_2, elmnt_3 = [1, "std", 4.5]
+print("elmnt_1:", elmnt_1)
+print("elmnt_2:", elmnt_2)
+
+#8
+
+#2person = "eliya"
+#print(2person)
+
+
+#9
+#@your_add = "Tehran"
+#print(@your_add)
+
+
+#10
+student_mark = 14
+student_age = 12.5
+print(type(student_mark))
+print(type(student_age))
+
+#casting
+int_var = 12
+float_var = 12.5
+
+int_to_float_var = float(int_var)
+print(type(int_to_float_var))
+print(int_to_float_var)
